30ce7143c02425b6d7fc4522539659fd18abaf53
nithin <nithin5454m@gmail.com>
2024-10-09 09:29:20 +0530

    Basic setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar/Navbar'
 const App = () => {
   return (
     <div className='app'>
-      <Navbar/>
+      
     </div>
   )
 }
--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,5 +1,11 @@
 import search_icon from './search_icon.png'
+import profile_icon from './profile_icon.png'
+import logo from './logo.png'
+
 
 export const assets = {
     search_icon,
+    profile_icon,
+    logo,
+    
 }

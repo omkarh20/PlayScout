e52315d6b0b880d553826727d330054419eb30a4
Omkar S Hegde <omkarhegde20@gmail.com>
2024-10-22 15:51:21 +0530

    Changed color palette

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home'
 import Meet from './pages/Meet/Meet'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
+import Privacy from './pages/Privacy/Privacy'
 import Booking from './pages/Booking/Booking'
 
 const App = () => {
@@ -14,16 +15,15 @@ const App = () => {
   return (
     <>
     {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
-      <div className='app'>
+      <div className='app' style={{backgroundColor:'#f1f1f1'}}>
         <Navbar setShowLogin = {setShowLogin} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/book' element={<Booking/>}/>
           <Route path='/meet' element={<Meet />} />
-
         </Routes>
+        <Footer />
       </div>
-      <Footer />
     </>
   )
 }
